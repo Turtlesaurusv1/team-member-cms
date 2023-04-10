65041e584247664f89114dbd8cff844e81024e08
ngnshud <ngnshud@gmail.com>
2023-04-10 16:45:45 +0700

    first commit

---
+++ src/emplyees.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+const Employee = () => {
+  const [Employee, setEmployee] = useState([
+    {
+      id: 1,
+      fullName: "nut",
+      desigantion: "backend",
+      gender: "male",
+      teamName: "A",
+    },
+    {
+      id: 2,
+      fullName: "t",
+      desigantion: "backend",
+      gender: "male",
+      teamName: "A",
+    },
+    {
+      id: 3,
+      fullName: "ts",
+      desigantion: "backend",
+      gender: "male",
+      teamName: "A",
+    },
+  ]);
+  return <main>
+
+  </main>;
+};
+
+export default Employee;
